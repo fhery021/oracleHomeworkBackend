534818e22369aad6442f15ab79224f823755212d
Ferenc Bondar <fhery021@gmail.com>
2020-04-16 12:55:49 +0200

    first commit

---
+++ src/main/java/com/example/homeworkbackend/HomeworkbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkbackendApplication.class, args);
+	}
+
+}
